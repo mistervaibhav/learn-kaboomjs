f36939cb3700554a4f27ed66a5aaaf369bb28c65
mistervaibhav <>
2021-05-28 18:35:46 +0000

    Initial commit

---
+++ scenes/main.js
@@ -0,0 +1,32 @@
+const playerSprite = sprite('player')
+const size = scale(0.5)
+const position = pos(40,40)
+
+const player = [player , size , position]
+
+add(actor)
+
+const ground = 
+
+
+
+const map = [
+  '          ',
+  '          ',
+  '     @    ',
+  '          ',
+  'xxxxxxxxxx',
+  ]
+
+const config = {
+  width : 40 ,
+  height : 40,
+  'x' : 
+}
+
+addLevel(map , config)
+
+
+
+
+
